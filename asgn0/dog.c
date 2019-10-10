e997b9214f2e2ac076b51507c13ca42cd5d8e587
jesales <jesales@ucsc.edu>
2019-10-09 22:30:15 -0700

    edited Dog

--- asgn0/dog.c
+++ asgn0/dog.c
@@ -5,35 +5,52 @@
 // the cat function on termninal.
 
 #include <stdio.h>
+#include <unistd.h>
 
 int main (int arg_count, char *arg[]) {
-    int i;
+    int i, j, in;
     char *dash = "-";
+    char stdin_buf[1];
+    // This takes in stdin
+    // from command line
     // If "dog" is just enterned into the command line
     if (arg_count == 1) {
-        goto take_stdin;
+        in = read (STDIN_FILENO, stdin_buf, sizeof(stdin_buf));
+        while (in > 0) {                                 
+            write (STDOUT_FILENO, stdin_buf, sizeof(stdin_buf));          
+            in = read (STDIN_FILENO, stdin_buf, sizeof(stdin_buf));
+        }
     }
     // If more than one command is entered into command
     // line. can either be "-" or file name(s).
     else {
-        for(i = 1;i <= arg_count; i++) {
+        for(i = 1; i < arg_count; i++) {
             // If a dash is just entered
             if (*arg[1] == *dash) {
-                goto take_stdin;
+                in = read (STDIN_FILENO, stdin_buf, sizeof(stdin_buf));
+                while (in > 0) {                         // This takes in stdin
+                    write (1, stdin_buf, sizeof(stdin_buf));  // from command line
+                    in = read (STDIN_FILENO, stdin_buf, sizeof(stdin_buf));
+                }
             }
             // If a file name(s) are entered
             else {
-
+                if (arg_count > 2) {
+                    //for (j = 1; j < arg_count; j++ ) {
+                        // opens first file
+                        //int file = open (*arg[j], O_RDONLY);
+                        // print errors if no file name is found
+                    //}
+                    printf( "not programmed yet");
+                }
+                else {
+                    // opens first file
+                    //int file = open (*arg[1], O_RDONLY); 
+                    // print errors if no file name is found
+                    printf( "not programmed yet");
+                }
             }
         }
     }
-
-    // This takes in command line input and then
-    // writes it to standard output.
-    take_stdin:
-        char in[1];
-        while(read(0, in, sizeof(in)) > 0) {
-            write("in%c", buff, strlen(buff));
-        }
-    
 }
+
