83123d7fe43418117ff394a281c48f23e124cd58
jesales <jesales@ucsc.edu>
2019-10-11 12:31:01 -0700

    implemented changes from design doc to program

--- asgn0/dog.c
+++ asgn0/dog.c
@@ -3,12 +3,14 @@
 // ID: jesales
 // DESCRIPTION: A simple program that replicates
 // the cat function on termninal.
+// Resources used were piazza posts and man pages.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <err.h>
+#include <string.h>
 
 int main (int argc, char *argv[]) {
     int i, j, in;
@@ -32,53 +34,38 @@ int main (int argc, char *argv[]) {
             // If a dash is just entered
             if (*argv[1] == *dash) {
                 in = read (STDIN_FILENO, stdin_buf, sizeof(stdin_buf));
-                while (in > 0) {                         // This takes in stdin
-                    write (STDOUT_FILENO, stdin_buf, sizeof(stdin_buf));  // from command line
+                while (in > 0) {                              // This takes in stdin
+                    write (1, stdin_buf, sizeof(stdin_buf));  // from command line
                     in = read (STDIN_FILENO, stdin_buf, sizeof(stdin_buf));
                 }
             }
             // If a file name(s) are entered
             else {
                 if (argc > 1) {
-                    for (j = 1; j < argc; j++ ) {
-                        // opens first file
-                        // print errors if no file name is found
-                        int file = open (argv[j], O_RDONLY);
-                        char file_buf[32768];   // sizeof(*arg[j])
-                        //char *file_name = arg[j];
-                        if ( file == -1 ){
-                            // print errors
-                            warn ("%s", argv[j]);
-                            //printf ("dog: %s: No such file or directory", );
+                    // opens first file
+                    // print errors if no file name is found
+                    int file = open (argv[i], O_RDONLY);
+                    const int buf_size = 32768;    // Someone has said to
+                    char file_buf[buf_size];       // to do this on piazza
+                    // print error is file is not found
+                    if ( file == -1 ){
+                        warn ("%s", argv[i]);
+                        exit (1);
+                    }
+                    else {
+                        int file_in = read (file, file_buf, buf_size);
+                        // print error if file cannot be read
+                        if (file_in == -1){
+                            warn ("%s", argv[i]);
                             exit (1);
-                            //warn ("dog: %s: No such file or directory", *file_name);
                         }
                         else {
-                            int file_in = read (file, file_buf, sizeof(file_buf));
-                            if (file_in < 0){
-                                printf ("Error: cannot read file");
-                                exit (1);
-                            }
-                            else {
-                                write (STDOUT_FILENO, file_buf, sizeof(file_buf));
-                                close(in);
-                                close (file); 
-                            } 
-                            //printf ("%d\n", file_in);
-                            //while (file > 0) {
-                                //printf ("file\n");                       
-                                //write (1, file_buf, sizeof(file_buf));  //
-                                //file_in = read (*arg[j], file_buf, sizeof(file_buf));
-                            //}
-                        }
+                            write (1, file_buf, file_in);
+                            close(in);
+                            close (file); 
+                        } 
                     }
                 }
-                //else {
-                    // opens first file
-                    //int file = open (*arg[1], O_RDONLY); 
-                    // print errors if no file name is found
-                    //printf( "not programmed yet");
-                //}
             }
         }
     }
