2b8a0bc6190d32c5118c138f496f47a762237dd2
jesales <jesales@ucsc.edu>
2019-10-28 02:22:35 -0700

    edited server.cpp

--- asgn1/server.cpp
+++ asgn1/server.cpp
@@ -21,10 +21,11 @@ int main (int argc, char *argv[]) {
     // Status codes and messages
     char *ok = "HTTP/1.1 200 OK\n";
     char *created = "HTTP/1.1 201 Created\n";
-    char *bad_request = "HTTP/1.1 400 Bad Request\n";
+    char *bad_request = "HTTP/1.1 400 Bad request\n";
     char *forbidden = "HTTP/1.1 403 Forbidden\n";
     char *file_not_found = "HTTP/1.1 404 Not Found\n";
     char *internal_server_error = "HTTP/1.1 500 Internal Server Error\n";
+    char *put_continue = "HTTP/1.1 100 Continue\n\n";
 
     // Create sockaddr_in
     struct hostent *hent = gethostbyname(SERVER_NAME_STRING);
@@ -51,28 +52,48 @@ int main (int argc, char *argv[]) {
     listen(sock, 0);
 
     const int buf_size = 32768;
+    //printf("------------------started server-------------------\n");
     while (1){
+        //printf("------------------waiting for connection-------------------\n");
         int cl = accept(sock, NULL, NULL);
         if (cl<0) {
             perror("In accept");
             exit(EXIT_FAILURE);
         }
-        printf("------------------started server-------------------\n");
         char buffer[buf_size] = {0};
         char get_buff[buf_size];
         char file_buff[buf_size];
         char header1[3];            // contains request
         char header2[28];           // contains target
         char header3[15];           // contains HTTP/1.1
+        char header4[64];           // contains Content-length line
         char get[] = "GET";
         char put[] = "PUT";
         char slash[] = "/";
-        int content_len;
+        int content_len = 0;
         // Read to get header
         int valread = read(cl, buffer, buf_size);
     
         // Parse header
-        sscanf(buffer, "%s %s %s", header1, header2, header3);   
+        sscanf(buffer, "%s %s %s", header1, header2, header3);
+        char * line = strtok(strdup(buffer), "\n");
+        int first_line = 1;
+        while (line != NULL) {
+            //printf("DEBUG line: %s\n", line);
+            // check for GET or PUT if first line in buffer 
+            if ( first_line == 1 ) {
+                sscanf(line, "%s %s %s", header1, header2, header3);
+                //printf("DEBUG: 1:%s 2:%s 3:%s\n", header1, header2, header3);
+                first_line = 0;
+            }
+            if (strncmp(line, "Content-Length:", 15) == 0) {
+                sscanf(line, "%s %d", header4, &content_len);
+                //printf("line: %s %d\n", header4, content_len);
+            } 
+            // add other parsers here
+            line = strtok(NULL, "\n");
+        }
+
         if (*header1 == *get) {
             int file_fetch;
             int file = open(header2, O_RDONLY);
@@ -114,15 +135,74 @@ int main (int argc, char *argv[]) {
             close(file_fetch);
         }
         if (*header1 == *put) {
-            //write(cl, buffer, strlen(buffer));
-            sprintf(buffer, "got PUT\n");
-            // writes out header to client
-            write(cl , buffer , strlen(buffer));
+            int to_file;
+            // If "/" then curl is called without target
+            // Print 400 message because there is no target
+            if (*header2 == *slash){
+                //printf("header2: %s\n", header2);
+                write(cl, forbidden, strlen(forbidden));
+                sprintf(get_buff, "Content-length: 0\n");
+                write(cl, get_buff, strlen(get_buff));
+            }
+            // If file does not exist, print 403 message
+            else {
+                int fd = open(header2, O_RDWR | O_CREAT | O_TRUNC, 644);
+                if (fd == -1) {
+                    // fail to open file for creation
+                    write(cl, bad_request, strlen(bad_request));
+                    sprintf(get_buff, "Content-length: 0\n");
+                    write(cl, get_buff, strlen(get_buff));
+                }
+                // If file exists is over written or written too
+                else {
+                    int write_to_file;
+                    write(cl , put_continue , strlen(put_continue));
+                    int putread = read(cl, file_buff, buf_size);
+                    //printf("putread: %d\n",putread);
+                    int counter = content_len;
+                    //printf("DEBUG: file_buff = %s\n", file_buff);
+                    while(counter >= buf_size) {
+                        write(fd, file_buff, putread);
+                        putread = read(cl, file_buff, putread);
+                        counter = counter - buf_size;
+                        //printf("DEBUG: file_buff = %s\n", file_buff);
+                        //printf("loop\n");
+                        //printf("putread: %d\n",putread);
+                        //printf("counter: %d\n", counter);
+                    }
+                    //printf("DEBUG: file_buff = %s\n", file_buff);
+                    //printf("out of loop\n");
+                    write(fd,file_buff, putread);
+                    write(cl, created, strlen(created));
+                    sprintf(get_buff, "Content-length: 0\n");
+                    write(cl, get_buff, strlen(get_buff));
+                    close(fd);
+                }
+            }
         }
 
-        printf("---------------------message sent----------------------\n");
+        //printf("---------------------message sent----------------------\n");
         close(cl);
     }
+
+/*if (*header1 == *put) {
+            sprintf(buffer, "got PUT\n");
+            write(cl , put_continue , strlen(put_continue));
+            int putread = read(cl, put_buff, buf_size);
+            printf("DEBUG: put_buff = %s\n", put_buff);
+
+            // if file doesn't exist: create new file named: header2 with put_buff
+            //   if create file error, return 403 forbidden
+            //    else write(cl, created, strlen(created))
+            // else if file exists overrite it
+            //   if overrite error, return 403 forbidden 
+            //    else write(cl, created, strlen(created))
+            // else return a 500 server error
+
+            write(cl , created , strlen(created));
+
+       }*/
+
 
     return 0;
 
